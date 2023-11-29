faa6105ea8e1650264b79a95c34d7f6f550b7c65
Yuchao Zhang <418121364@qq.com>
2023-11-29 17:11:35 +0800

    Implement SSE streaming for completion (#3)
    * Implement SSE streaming
    
    * Add test client
    
    ---------
    
    Co-authored-by: npuzhangyuchao <yuchao.zhang@ibyteai.com>

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,10 @@
-use std::fmt::Debug;
-
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
 
+#[derive(Debug)]
 pub struct AppError(anyhow::Error);
-
-impl Debug for AppError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.0.fmt(f)
-    }
-}
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
