60cfa2f6bdefecf48ba01258a7ada2e57d4acfa8
Yuchao Zhang <418121364@qq.com>
2023-12-07 21:53:44 +0800

    Add more openai compatible parameters (#13)
    Co-authored-by: npuzhangyuchao <yuchao.zhang@ibyteai.com>

--- src/routes/completions.rs
+++ src/routes/completions.rs
@@ -178,7 +178,7 @@ async fn completions(
 }
 
 fn build_triton_request(request: CompletionCreateParams) -> anyhow::Result<ModelInferRequest> {
-    Builder::new()
+    let mut builder = Builder::new()
         .model_name(request.model)
         .input(
             "text_input",
@@ -204,15 +204,46 @@ fn build_triton_request(request: CompletionCreateParams) -> anyhow::Result<Model
         .input(
             "stop_words",
             [1, 1],
-            InferTensorData::Bytes(vec!["</s>".as_bytes().to_vec()]),
+            InferTensorData::Bytes(
+                request
+                    .stop
+                    .unwrap_or_else(|| vec!["</s>".to_string()])
+                    .into_iter()
+                    .map(|s| s.into_bytes())
+                    .collect(),
+            ),
+        )
+        .input("top_p", [1, 1], InferTensorData::FP32(vec![request.top_p]))
+        .input(
+            "temperature",
+            [1, 1],
+            InferTensorData::FP32(vec![request.temperature]),
+        )
+        .input(
+            "presence_penalty",
+            [1, 1],
+            InferTensorData::FP32(vec![request.presence_penalty]),
+        )
+        .input(
+            "beam_width",
+            [1, 1],
+            InferTensorData::Int32(vec![request.n as i32]),
         )
         .input(
             "stream",
             [1, 1],
             InferTensorData::Bool(vec![request.stream]),
-        )
-        .build()
-        .context("failed to build triton request")
+        );
+
+    if request.seed.is_some() {
+        builder = builder.input(
+            "random_seed",
+            [1, 1],
+            InferTensorData::UInt64(vec![request.seed.unwrap() as u64]),
+        );
+    }
+
+    builder.build().context("failed to build triton request")
 }
 
 #[allow(dead_code)]
