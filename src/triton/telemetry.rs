a33cf441a7c7eef1f795c3dbfaec23f55bcc3d92
npuichigo <418121364@qq.com>
2024-03-12 12:30:58 +0800

    Propagate telemetry context

--- src/triton/telemetry.rs
+++ src/triton/telemetry.rs
@@ -2,6 +2,8 @@ use axum::http::HeaderMap;
 use opentelemetry::propagation::Injector;
 use opentelemetry::{global, Context};
 use tonic::Request;
+use tracing::Span;
+use tracing_opentelemetry::OpenTelemetrySpanExt;
 
 pub struct MetadataMap<'a>(&'a mut tonic::metadata::MetadataMap);
 
@@ -18,10 +20,11 @@ impl<'a> Injector for MetadataMap<'a> {
 
 pub(crate) fn propagate_context<T>(request: &mut Request<T>, header: &HeaderMap) {
     let mut metadata_map = MetadataMap(request.metadata_mut());
+
     // Propagate the current opentelemetry context
-    global::get_text_map_propagator(|propagator| {
-        propagator.inject_context(&Context::current(), &mut metadata_map)
-    });
+    let cx = Span::current().context();
+    global::get_text_map_propagator(|propagator| propagator.inject_context(&cx, &mut metadata_map));
+
     // Propagate x-request-id header to the request if it exists
     if let Some(x_request_id) = header.get("x-request-id") {
         if let Ok(x_request_id) = x_request_id.to_str() {
